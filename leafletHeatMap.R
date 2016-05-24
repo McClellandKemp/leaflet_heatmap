773770ad06cb6a03dd1e74d41d714a09f16b5800
McClelland Kemp <McClelland.Kemp@iriworldwide.com>
2016-05-24 11:12:54 -0500

    update plugin source hrefs

--- leafletHeatMap.R
+++ leafletHeatMap.R
@@ -7,12 +7,16 @@ leafletHeatMapOutput <- function(inputId, width="100%", height="400px") {
       tags$head(
         tags$link(rel = "stylesheet", type = "text/css",
                   href = "http://cdn.leafletjs.com/leaflet/v0.7.7/leaflet.css"),
-        tags$link(rel = "stylesheet", type = "text/css",
-                  href = "http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"),
-        tags$script(src = "http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"),
+        tags$link(rel = "stylesheet",
+                  href = "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css",
+                  integrity = "sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7",
+                  crossorigin = "anonymous"),
+        tags$script(src = "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js",
+                    integrity = "sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS",
+                    crossorigin = "anonymous"),
         tags$script(src = "http://cdn.leafletjs.com/leaflet/v0.7.7/leaflet.js"),
         tags$script(src = "plugins/Leaflet.heat-gh-pages/dist/leaflet-heat.js"),
-        tags$script(src = "plugins/master/dist/Leaflet.Coordinates-0.1.5.src.js"),
+        tags$script(src = "leafletheatmap-binding.js"),
         tags$style(type = "text/css",
                    "#leaflet-heat-map {
                      width: 100%;
